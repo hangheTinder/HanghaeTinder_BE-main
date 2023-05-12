2fd657f0ff110ee03dce2a8b6877b50d9c1b9edb
Cho-woo-pil <jwp4465@naver.com>
2023-05-12 18:52:43 +0900

    feat : chat기능을 제외한 entity설계

---
+++ src/main/java/com/example/hanghaetinder_bemain/entity/MemberFavorite.java
@@ -0,0 +1,28 @@
+package com.example.hanghaetinder_bemain.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "member_favorite")
+public class MemberFavorite {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@ManyToOne
+	@JoinColumn(name = "favorite_id")
+	private Favorite favorite;
+
+}
