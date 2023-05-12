6e5ca7381c05bcb8c426dbdb46931a8eb51678d8
Cho-woo-pil <jwp4465@naver.com>
2023-05-12 13:56:19 +0900

    Initial commit

---
+++ src/main/java/com/example/hanghaetinder_bemain/HanghaeTinderBeMainApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanghaetinder_bemain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeTinderBeMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanghaeTinderBeMainApplication.class, args);
+	}
+
+}
