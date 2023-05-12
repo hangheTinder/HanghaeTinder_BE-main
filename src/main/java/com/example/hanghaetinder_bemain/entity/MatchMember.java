1a3ee39b5a377627660de828593ff293deb8bb71
Cho-woo-pil <jwp4465@naver.com>
2023-05-12 18:52:07 +0900

    feat : chat기능을 제외한 entity설계

---
+++ src/main/java/com/example/hanghaetinder_bemain/entity/MatchMember.java
@@ -0,0 +1,27 @@
+package com.example.hanghaetinder_bemain.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "match_member")
+public class MatchMember {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@ManyToOne
+	@JoinColumn(name = "match_member_id")
+	private Member matchMember;
+
+}
--- src/main/java/com/example/hanghaetinder_bemain/entity/Member.java
+++ src/main/java/com/example/hanghaetinder_bemain/entity/Member.java
@@ -1,10 +1,19 @@
 package com.example.hanghaetinder_bemain.entity;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,21 +29,41 @@ public class Member extends Timestamped {
 	private Long id;
 
 	@Column(nullable = false)
-	private String username;
+	private String userId;
 
 	@Column(nullable = false)
 	private String password;
 
 	@Column(nullable = false)
-	private String email;
+	private String nickname;
 
-	private Member(String username, String password, String email) {
-		this.username = username;
-		this.password = password;
-		this.email = email;
-	}
+	@Column(nullable = false)
+	private LocalDate birth;
 
-	public static Member of(String username, String password, String email) {
-		return new Member(username, password, email);
-	}
+	@Column(nullable = false)
+	private int age;
+
+	@Column(nullable = false)
+	private String gender;
+
+	@Column(nullable = false)
+	private String img;
+
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(
+		name = "member_favorite",
+		joinColumns = @JoinColumn(name = "member_id"),
+		inverseJoinColumns = @JoinColumn(name = "favorite_id")
+	)
+	private Set<Favorite> favorites = new HashSet<>();
+
+	@OneToMany(mappedBy = "member")
+	private Set<Member> likeMembers = new HashSet<>();
+
+	@OneToMany(mappedBy = "member")
+	private Set<Member> dislikeMember = new HashSet<>();
+
+	@OneToMany(mappedBy = "member")
+	private Set<Member> matchMember = new HashSet<>();
+
 }
