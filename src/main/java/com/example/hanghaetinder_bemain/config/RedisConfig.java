c3ef34dcec5ac3dd63a4794e922f42e607d778d5
Cho-woo-pil <jwp4465@naver.com>
2023-05-14 16:42:20 +0900

    refactor : Redis Config 리팩토링

--- src/main/java/com/example/hanghaetinder_bemain/config/RedisConfig.java
+++ src/main/java/com/example/hanghaetinder_bemain/config/RedisConfig.java
@@ -31,23 +31,6 @@ public class RedisConfig {
 	public RedisConnectionFactory redisConnectionFactory() {
 		return new LettuceConnectionFactory(redisHost, redisPort);
 	}
-
-	@Bean
-	public RedisTemplate<?, ?> redisTemplate() {
-		RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
-		// 아래 두 라인을 작성하지 않으면, key값이 \xac\xed\x00\x05t\x00\x03sol 이렇게 조회된다.
-		redisTemplate.setKeySerializer(new StringRedisSerializer());
-		redisTemplate.setValueSerializer(new StringRedisSerializer());
-		redisTemplate.setConnectionFactory(redisConnectionFactory());
-		return redisTemplate;
-	}
-
-}
-
-		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
-		return new LettuceConnectionFactory(redisStandaloneConfiguration);
-	}
-
 	@Bean
 	public RedisTemplate<String, ChatMessage> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
 		RedisTemplate<String, ChatMessage> template = new RedisTemplate<>();
@@ -65,4 +48,6 @@ public class RedisConfig {
 		return template;
 	}
 
+
 }
+
