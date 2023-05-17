b1d6b79dac34bf2631c85f9ba1c48046ad1ff072
Cho-woo-pil <jwp4465@naver.com>
2023-05-17 22:26:05 +0900

    fix : react 배포 cors 추가

--- src/main/java/com/example/hanghaetinder_bemain/domain/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/hanghaetinder_bemain/domain/security/config/WebSecurityConfig.java
@@ -80,6 +80,7 @@ public class WebSecurityConfig {
 
 		config.addAllowedOrigin("http://localhost:3000");
 		config.addAllowedOrigin("http://localhost:8080");
+		config.addAllowedOrigin("http://hanghae-tinder-clone.s3-website.ap-northeast-2.amazonaws.com:3000");
 		config.addExposedHeader(JwtUtil.AUTHORIZATION_HEADER);
 		config.addAllowedMethod("*");
 		config.addAllowedHeader("*");
